c8228a113bae7d20ee7a6939ddfd3f6b9fb51f07
Bharath Kiran Obilisetty <bharathkiran2422@gmail.com>
2025-08-07 10:24:08 +0000

    const firebaseConfig = {   apiKey: "AIzaSyBSCS-gcpk3akUSE5Uh7A96LoW1zh66

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "gerald-portfolio-crsb1",
-  "appId": "1:178028773105:web:fb1b7ba8fc8f0b2e48f350",
-  "storageBucket": "gerald-portfolio-crsb1.firebasestorage.app",
-  "apiKey": "AIzaSyC99DlZik6RuzS8fyElv9RXW0E3twzhgqg",
-  "authDomain": "gerald-portfolio-crsb1.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "178028773105"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 

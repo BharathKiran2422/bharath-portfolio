e3f6459aed2e29357c4d2d99f511795d1c088579
Bharath Kiran Obilisetty <bharathkiran2422@gmail.com>
2025-08-08 09:42:54 +0000

    Use the provided cursor icon sets to create a cohesive, theme-aware poin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
+import CustomCursor from '@/components/custom-cursor';
 
 export const metadata: Metadata = {
   title: 'Bharath Kiran',
@@ -28,6 +29,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <CustomCursor />
           {children}
           <Toaster />
         </ThemeProvider>
